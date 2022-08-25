5858c8b4b3e3f92ac4892474bc6138ca801bda33
Mohammad Zarifiyan <mohammadzarifiyan82@gmail.com>
2022-08-25 21:37:28 +0430

    Fix bug

--- src/Exceptions/TelegramOriginException.php
+++ src/Exceptions/TelegramOriginException.php
@@ -3,8 +3,12 @@
 namespace MohammadZarifiyan\Telegram\Exceptions;
 
 use Exception;
+use Symfony\Component\HttpFoundation\Response;
 
 class TelegramOriginException extends Exception
 {
-	//
+	public function render()
+	{
+		return response(status: Response::HTTP_UNAUTHORIZED);
+	}
 }
