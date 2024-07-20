d09574e8704b57cdc231db731b9725734a834da5
Mohammad Zarifiyan <mohmmadzarifiyan82@gmail.com>
2024-07-20 09:53:49 +0330

    Fixing the problem of function name interference

--- src/helpers.php
+++ src/helpers.php
@@ -3,6 +3,7 @@
 namespace MohammadZarifiyan\Telegram;
 
 use Closure;
+use InvalidArgumentException;
 
 if (!function_exists('try_resolve')) {
 	function try_resolve($class)
@@ -15,24 +16,22 @@ if (!function_exists('try_resolve')) {
 	}
 }
 
-
 if (!function_exists('array_map_recursive')) {
-    function array_map_recursive(array $array, Closure $callback): array
+    function array_map_recursive(array $array, Closure $callback, int $depth = 0): array
     {
-        function recursive(array $array, Closure $callback, int $depth): array
-        {
-            foreach ($array as $key => $value) {
-                if (is_array($value)) {
-                    $array[$key] = recursive($value, $callback, $depth + 1);
-                }
-                else {
-                    $array[$key] = call_user_func($callback, $value, $key, $depth);
-                }
-            }
-
-            return $array;
+        if ($depth < 0) {
+            throw new InvalidArgumentException('Depth cannot be negative');
         }
 
-        return recursive($array, $callback, 0);
+        foreach ($array as $key => $value) {
+            if (is_array($value)) {
+                $array[$key] = array_map_recursive($value, $callback, $depth + 1);
+            }
+            else {
+                $array[$key] = call_user_func($callback, $value, $key, $depth);
+            }
+        }
+
+        return $array;
     }
 }
