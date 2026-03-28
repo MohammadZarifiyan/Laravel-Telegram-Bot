c7320a6deec58eec264ff2aef015895bda7b613a
Mohammad <mohammadzarifiyan82@gmail.com>
2026-03-28 19:47:40 +0330

    Make Attachment class properties readonly

--- src/Attachment.php
+++ src/Attachment.php
@@ -5,9 +5,9 @@ namespace MohammadZarifiyan\Telegram;
 class Attachment
 {
     public function __construct(
-        public $content,
-        public string $filename,
-        public array $headers = []
+        public readonly mixed $content,
+        public readonly string $filename,
+        public readonly array $headers = []
     ) {
         //
     }
